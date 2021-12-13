265a4ec51eb3a658739a72815db6bf9ca4a4a150
oussama24bessaad <93039035+oussama24bessaad@users.noreply.github.com>
2021-12-13 15:40:57 +0100

    Add files via upload

---
+++ node_modules/App.js
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import React from "react";
+import SignUp from "./SignUp";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <SignUp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
